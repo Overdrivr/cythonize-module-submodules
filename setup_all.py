0de6c156014c2698dd71f0ef8114e763142cc471
Remi Beges <remi.beges@gmail.com>
2018-07-04 10:54:25 +0200

    Fix Step 6

--- setup_all.py
+++ setup_all.py
@@ -21,25 +21,28 @@ def cleanup_build_outputs():
     except:
         pass
 
+def search_files_recursively(root_dir, pattern):
+    '''Searches files inside subdirectories, including root directory
+
+    '''
+    scan_root = os.path.join(root_dir, pattern)
+    files = glob(scan_root)
+    scan_root = os.path.join(root_dir, '**' , pattern)
+    files += glob(scan_root)
+    return files
+
 def scan_sources(root_dir):
     '''
     Scans the entire folder, retrieving all python files except
     __init__ files (see PROCESS.md step 1).
     '''
-    # Search in root dir
-    scan_root = os.path.join(root_dir, '*.py')
-    print('Scanning files in ', scan_root)
-    files = glob(scan_root)
+    print('Scanning files in ', root_dir)
+    files = search_files_recursively(root_dir, '*.py')
 
-    # Search in sub-dirs
-    scan_root = os.path.join(root_dir, '**' , '*.py')
-    print('Scanning files in ', scan_root)
-    files += glob(scan_root)
-
-    #files = [f for f in files if os.path.basename(f) != '__init__.py']
+    # Remove __init__.py from compilation, otherwise link error
+    files = [f for f in files if os.path.basename(f) != '__init__.py']
     print('Detected for compilation:', files)
     return files
-
 
 # For removing build/dist folders (to have a clean build env everytime)
 cleanup_build_outputs()
@@ -51,6 +54,28 @@ ext_modules = cythonize(scan_sources('mypkg'),
     }
 )
 
+class MyBuildExt(build_ext):
+    def run(self):
+        super(MyBuildExt, self).run()
+
+        root_dir = os.getcwd()
+        build_dir = os.path.join(root_dir, self.build_lib)
+        target_dir = build_dir if not self.inplace else root_dir
+
+        # Identify all init.py
+        scan_root = os.path.join(root_dir, 'mypkg')
+        pattern = '__init__.py'
+        print('Searching for __init__.py in ', scan_root)
+        files = search_files_recursively(scan_root, pattern)
+
+        for f in files:
+            diff = os.path.commonpath([root_dir, f])
+            diff = f.replace(diff, '')
+            dest = target_dir + diff
+            import pdb; pdb.set_trace()
+            print('cp %s -> %s' % (f, dest))
+            shutil.copyfile(f, dest)
+
 setup(
     name='mypkg',
     version='0.1.0',
@@ -61,6 +86,6 @@ setup(
     packages=[],
     ext_modules = ext_modules,
     cmdclass = {
-        'build_ext': build_ext
+        'build_ext': MyBuildExt
     }
 )
